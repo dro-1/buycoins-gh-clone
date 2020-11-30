63cd4f526cae46ac8c07ef80fecd4cc0bbd12bb4
dro-1 <lorddro1532@gmail.com>
2020-11-30 15:47:25 +0100

    Done with responsiveness

--- script.js
+++ script.js
@@ -4,3 +4,38 @@ tabs.forEach((tab) => {
     evt.target.classList.add("active");
   });
 });
+
+const isVisible = (elem) => {
+  var distance = elem.getBoundingClientRect();
+  return (
+    distance.top >= 50 &&
+    distance.left >= 0 &&
+    distance.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    distance.right <=
+      (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+const nav = document.querySelector(
+  "section.profile-container header div.profile p span:last-child"
+);
+
+const navProfile = document.querySelector("div.nav-profile");
+
+window.onscroll = () => {
+  if (
+    nav &&
+    !isVisible(nav) &&
+    window.matchMedia("(min-width: 768px)").matches
+  ) {
+    navProfile.classList.add("active");
+  }
+  if (
+    nav &&
+    isVisible(nav) &&
+    window.matchMedia("(min-width: 768px)").matches
+  ) {
+    navProfile.classList.remove("active");
+  }
+};
